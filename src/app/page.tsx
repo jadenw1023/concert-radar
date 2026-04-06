05186f81a327e702342a4f66ebe767add0634275
jadenw1023 <jaysmuvv2@gmail.com>
2026-04-05 21:02:59 -0400

    feat: display login state with session provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  console.log("Session status:", status);
+  console.log("Session data:", session);
+
+  if (session) {
+    return (
+      <main>
+        <h1>Concert Radar</h1>
+        <p>Welcome, {session.user?.name}</p>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </main>
+    );
+  }
+
   return (
     <main>
       <h1>Concert Radar</h1>
-      <p>Find concerts from your favorite spotify artists!</p>
+      <button onClick={() => signIn("spotify")}>Connect with Spotify</button>
     </main>
-  )
+  );
 }
-
